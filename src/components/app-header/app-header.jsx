9b9a17481c6510f66621682425a1a9e39ccccdf9
Svetlana <svetapav@yandex.ru>
2022-09-02 16:40:50 +0300

    add AppHeader component

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,13 +1,31 @@
 
 import React from 'react' // импорт библиотеки
-
+import {BurgerIcon} from '@ya.praktikum/react-developer-burger-ui-components'
+import {ListIcon } from '@ya.praktikum/react-developer-burger-ui-components'
+import {ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components'
+import {Logo} from '@ya.praktikum/react-developer-burger-ui-components'
 
 class AppHeader extends React.Component {
     render() {
       return (
-              <h1>
-                  AppHeader
-              </h1>
+        <div className="AppHeader">
+              <BurgerIcon type="primary" />
+              <span className="text text_type_main-default">
+                Конструктор
+              </span>
+
+              <ListIcon type="secondary" />
+              <span className="text text_type_main-default">
+                Лента заказов
+              </span>
+
+              <Logo />
+
+              <ProfileIcon type="secondary" />
+              <span className="text text_type_main-default">
+                Личный кабинет
+              </span>
+        </div>
       );
     }
   }
