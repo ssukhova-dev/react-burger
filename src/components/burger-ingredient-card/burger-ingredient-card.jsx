ce5e7d3752d47202232f03d29c3e4aff2f041add
Svetlana <svetapav@yandex.ru>
2022-09-07 16:05:54 +0300

    BurgerIngredientCard функциональный компонент

--- src/components/burger-ingredient-card/burger-ingredient-card.jsx
+++ src/components/burger-ingredient-card/burger-ingredient-card.jsx
@@ -7,32 +7,32 @@ import burgerIngredientCard from './burger-ingredient-card.module.css';
 import '@ya.praktikum/react-developer-burger-ui-components'
 import {CurrencyIcon, Counter} from '@ya.praktikum/react-developer-burger-ui-components'
 
-class BurgerIngredientCard extends React.Component {
-    render() {
+function BurgerIngredientCard(props) {
+
       return (
               <div className={burgerIngredientCard.burger_ingredient_card}>
 
          
                 <Counter count={1} size="default" />
 
-                <img src={ this.props.thumbnail } alt={`изображение ингредиента ${this.props.text}`} />
+                <img src={ props.thumbnail } alt={`изображение ингредиента ${props.text}`} />
                
                 <span className={`ml-2 mr-2 mb-2 mt-2 ${burgerIngredientCard.flex_center_row}`} >
                     <p className="text text_type_main-small"  >
-                        {this.props.price}
+                        {props.price}
                     </p>
                     <CurrencyIcon type="primary" />
                 </span>              
 
                 <p className="text text_type_main-small" >
-                    {this.props.text}
+                    {props.text}
                 </p>
                   
               </div>
 
 
       );
-    } 
+  
   }
 
   BurgerIngredientCard.propTypes = {
