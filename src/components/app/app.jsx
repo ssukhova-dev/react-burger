b285e48ac6e626042e04596c92879d2c23f27d1a
Svetlana <svetapav@yandex.ru>
2022-09-07 16:59:31 +0300

    получение реальных данных

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -5,20 +5,45 @@ import AppHeader from '../app-header/app-header'
 import BurgerIngredients from '../burger-ingredients/burger-ingredients'
 import BurgerConstructor from '../burger-constructor/burger-constructor'
 
-import appData from '../../utils/data.js';
-
-
+//import appData from '../../utils/data.js';
 //import '@ya.praktikum/react-developer-burger-ui-components' <div className="App ml-5 mr-5 mb-5 mt-5">
 
 function App() {
+
+  const [state, setState] = React.useState({isLoading: false,
+    hasError: false,
+    ingredients: []});
+
+  const ingredientsUrl = "https://norma.nomoreparties.space/api/ingredients";
+
+  React.useEffect(()=>{ 
+    const getIngredients = async () => {
+      setState({ ...state, hasError: false, isLoading: true });
+      fetch(ingredientsUrl)
+        .then(res => res.json())
+        .then(data => {
+          
+              console.log(data);
+              setState({ ...state, ingredients: data.data, isLoading: false }) 
+
+            }
+          )
+        .catch(e => {
+          setState({ ...state, hasError: true, isLoading: false });
+        });
+      }
+
+      getIngredients();
+  }, []);
+
 
   return (
     <div className={styles.app}>
    
       <AppHeader/>
       <section className={styles.app_container}>
-        <BurgerIngredients ingredients = {appData}/>
-        <BurgerConstructor ingredients = {appData}/>
+        <BurgerIngredients ingredients = {state.ingredients}/>
+        <BurgerConstructor ingredients = {state.ingredients}/>
       </section>
 
    
