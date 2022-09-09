0c868649b018a6b4749d70cb902bd72d9568a67e
Svetlana <svetapav@yandex.ru>
2022-09-10 01:02:03 +0300

    полное отображение IngredientDetails

--- src/components/ingredient-details/ingredient-details.jsx
+++ src/components/ingredient-details/ingredient-details.jsx
@@ -4,8 +4,6 @@ import ingredientPropType from './../../utils/prop-types.jsx'
 
 import ingredientDetailsStyle from './ingredient-details.module.css';
 
-import {CurrencyIcon} from '@ya.praktikum/react-developer-burger-ui-components'
-
 import Modal from '../modal/modal'
 
 function IngredientDetails (props){
@@ -19,9 +17,29 @@ function IngredientDetails (props){
                <div className={ingredientDetailsStyle.content_body}>
                     <img src={ props.ingredient.image_large } alt={`изображение ингредиента ${props.ingredient.name}`} />         
 
-                    <p className="text text_type_main-medium" >
+                    <p className="text text_type_main-medium m-5" >
                         {props.ingredient.name}
                     </p>
+
+                    <ul className={ingredientDetailsStyle.nutritional_value_list}>
+                        <li className={ingredientDetailsStyle.nutritional_value}>
+                            <p className="text text_type_main-default text_color_inactive m-4">Калории, ккал</p>
+                            <p className="text text_type_digits-default text_color_inactive">{props.ingredient.calories}</p>
+                        </li>
+                        <li className={ingredientDetailsStyle.nutritional_value}>
+                            <p className="text text_type_main-default text_color_inactive m-4">Белки, г</p>
+                            <p className="text text_type_digits-default text_color_inactive">{props.ingredient.proteins}</p> 
+                        </li>
+                        <li className={ingredientDetailsStyle.nutritional_value}>
+                            <p className="text text_type_main-default text_color_inactive m-4">Жиры, г</p>
+                            <p className="text text_type_digits-default text_color_inactive">{props.ingredient.fat}</p> 
+                        </li>
+                        <li className={ingredientDetailsStyle.nutritional_value}>
+                            <p className="text text_type_main-default text_color_inactive m-4" >Углеводы, г</p>
+                            <p className="text text_type_digits-default text_color_inactive">{props.ingredient.carbohydrates}</p> 
+                        </li>
+
+                    </ul>
                </div>
            </Modal>
    
