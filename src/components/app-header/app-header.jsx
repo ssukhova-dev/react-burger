55f84aa62e32d658e242d535cba073684b0fdd47
Svetlana <svetapav@yandex.ru>
2022-09-10 16:41:29 +0300

    обернула в тэг header

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -7,54 +7,56 @@ import {BurgerIcon, ListIcon, ProfileIcon, Logo} from '@ya.praktikum/react-devel
 function AppHeader() {
    
       return (
-        <nav className={appHeaderStyles.app_header}>
-
-            <div className={appHeaderStyles.navigation_panel}>
-
-                <div className={appHeaderStyles.navigation_link} >
-                <a href="#" className={appHeaderStyles.navigation_link} >
-                    <div className={appHeaderStyles.link_icon}><BurgerIcon type="secondary"/></div>
-                    <div className={appHeaderStyles.link_icon_hover}><BurgerIcon type="primary"/></div>
-                    <p className="text text_type_main-default">
-                        Конструктор
-                    </p>
-                </a>
-                </div> 
-
-                <div className={appHeaderStyles.navigation_link} >
-                <a href="#" className={appHeaderStyles.navigation_link} >
-                <div className={appHeaderStyles.link_icon}><ListIcon type="secondary"/></div>
-                <div className={appHeaderStyles.link_icon_hover}><ListIcon type="primary"/></div>
-                <p className="text text_type_main-default">
-                    Лента заказов
-                </p>
-                </a>
-                </div>
-
-            </div>
-
+        <header className={appHeaderStyles.app_header}>
+            <nav className={appHeaderStyles.app_nav}>
  
-            <div className="flex-center pl-5 pr-5 pb-5 pt-5" >
-              <Logo  />
-            </div>
+                <div className={appHeaderStyles.navigation_panel}>
 
+                    <div className={appHeaderStyles.navigation_link} >
+                    <a href="#" className={appHeaderStyles.navigation_link} >
+                        <div className={appHeaderStyles.link_icon}><BurgerIcon type="secondary"/></div>
+                        <div className={appHeaderStyles.link_icon_hover}><BurgerIcon type="primary"/></div>
+                        <p className="text text_type_main-default">
+                            Конструктор
+                        </p>
+                    </a>
+                    </div> 
 
+                    <div className={appHeaderStyles.navigation_link} >
+                    <a href="#" className={appHeaderStyles.navigation_link} >
+                    <div className={appHeaderStyles.link_icon}><ListIcon type="secondary"/></div>
+                    <div className={appHeaderStyles.link_icon_hover}><ListIcon type="primary"/></div>
+                    <p className="text text_type_main-default">
+                        Лента заказов
+                    </p>
+                    </a>
+                    </div>
 
-
-            <div className={appHeaderStyles.navigation_account}>
-                <div className={appHeaderStyles.navigation_link} >
-                <a href="#" className={appHeaderStyles.navigation_link} >
-                <div className={appHeaderStyles.link_icon}><ProfileIcon type="secondary"/></div>
-                <div className={appHeaderStyles.link_icon_hover}><ProfileIcon type="primary"/></div>
-                <p className="text text_type_main-default">
-                    Личный кабинет
-                </p>
-                </a>
                 </div>
-            </div>
-              
 
-        </nav>
+    
+                <div className="flex-center pl-5 pr-5 pb-5 pt-5" >
+                <Logo  />
+                </div>
+
+
+
+
+                <div className={appHeaderStyles.navigation_account}>
+                    <div className={appHeaderStyles.navigation_link} >
+                    <a href="#" className={appHeaderStyles.navigation_link} >
+                    <div className={appHeaderStyles.link_icon}><ProfileIcon type="secondary"/></div>
+                    <div className={appHeaderStyles.link_icon_hover}><ProfileIcon type="primary"/></div>
+                    <p className="text text_type_main-default">
+                        Личный кабинет
+                    </p>
+                    </a>
+                    </div>
+                </div>
+                
+
+            </nav>
+        </header>
       );
     
   }
