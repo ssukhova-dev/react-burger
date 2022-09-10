358569fd35049ae1fccaeb42e9a376ec12c46d0d
Svetlana <svetapav@yandex.ru>
2022-09-10 17:01:22 +0300

    визуальное отображение активной страницы в хедере

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -13,7 +13,7 @@ function AppHeader() {
                 <div className={appHeaderStyles.navigation_panel}>
 
                     <div className={appHeaderStyles.navigation_link} >
-                    <a href="#" className={appHeaderStyles.navigation_link} >
+                    <a href="#" className={appHeaderStyles.navigation_link} active_page="true">
                         <div className={appHeaderStyles.link_icon}><BurgerIcon type="secondary"/></div>
                         <div className={appHeaderStyles.link_icon_hover}><BurgerIcon type="primary"/></div>
                         <p className="text text_type_main-default">
